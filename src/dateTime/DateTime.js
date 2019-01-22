3d44ce07b33ffb6ae509ee83b7cb3a240d31972f
novaQuach <nova.quach@gmail.com>
2019-01-21 22:31:38 -0500

    time edit

--- src/dateTime/DateTime.js
+++ src/dateTime/DateTime.js
@@ -17,15 +17,34 @@ class DateTime extends Component {
         clearInterval(this.interval);
     }
 
-    render() {
+    getTimeWithoutSecs = () => {
         const date = this.state.date;
+        // this is a built in method returns a string that contains the hour: minute : second AM/PM
+        const rawTimeString = date.toLocaleTimeString();
+
+        // extracting only the hour and minute from the rawstring.
+        const HourMinuteString = rawTimeString.slice(0, 5);
+
+        // extracting the AM/PM part of the raw string
+        const AmPmString = rawTimeString.slice(8, 11);
+
+        // concatenating the hourminute string with AM/PM string to give what is displayed
+        const timeString = `${HourMinuteString} ${AmPmString}`;
+
+        return timeString;
+    };
+
+    render() {
+        // const date = this.state.date;
+
+        console.log(this.getTimeWithoutSecs());
+
         return (
             <div className="dateTime">
-                <time className="date">{date.toDateString()}</time>
-                <time className="time">{date.toLocaleTimeString()}</time>
+                <time className="date">{this.state.date.toDateString()}</time>
+                <time className="time">{this.getTimeWithoutSecs()}</time>
             </div>
         );
     }
 }
-
 export default DateTime;
